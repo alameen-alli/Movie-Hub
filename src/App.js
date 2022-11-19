3746344c747060ca2172bee4760d622af4512399
Alameen8739 <allialameen8739@gmail.com>
2022-11-19 13:58:44 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect } from "react";
+
+// 17915b2d
+// http://www.omdbapi.com/?i=tt3896198&apikey=17915b2d
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=17915b2d";
+
+const App = () => {
+  const searchMovies = async(title) => {
+    
+  };
+
+  useEffect(() => {}, []);
+
+  return <h1>App</h1>;
+};
+
+export default App;
